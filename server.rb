5429471a81a6a5c11c0b971d149ad06110bdec6f
Sean Darcy <darcys22@gmail.com>
2016-04-07 05:44:52 +1000

    transactions

--- server.rb
+++ server.rb
@@ -5,10 +5,19 @@ require 'json'
 require 'pry'
 
 file = File.read('genosis.json')
-company_file = JSON.parse(file)
+company_file = JSON.parse(file, :symbolize_names => true)
 
 #binding.pry
 
-get '/transaction' do
-  "Hello World"
-end
+#post '/transaction' do
+  #content_type :json
+  ##transaction = JSON.parse(params[:data],:symbolize_names => true)
+  #request.body.rewind
+  #transaction = JSON.parse(request.body.read, :symbolize_names => true)
+  #binding.pry
+  #transaction.to_json
+#end
+
+txn_file = File.read('transaction.json')
+transaction = JSON.parse(txn_file, :symbolize_names => true)
+binding.pry
