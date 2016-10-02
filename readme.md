5e24c2dbbb7a3af1ef64a90558fe0e126c2a734d
Sean Darcy <darcys22@gmail.com>
2016-10-02 16:03:26 +1100

    reads from mongo and creates TB

--- readme.md
+++ readme.md
@@ -3,11 +3,15 @@
 just run main.rb to get to the webserver running. post a transaction to localhost:4567/transaction to get to binding.pry. That will leave you in the position wehre you can view the company file. it will have a transactions array with the recent transaction.
 
 transaction examples in data
+curl -v -X POST -d @./data/transaction.json localhost:4567/transaction --header "Content-Type:text/json"    
 
-in tools you can use Transactioner.new() to create a new file
+in tools you can use Transactioner.new() to create a new file (For a transaction)
 
 tools.rb signs transactions with the test keys
 
 TODO
 create the toolset for viewing TB and shit
+  read in information from file
+  refine TB to do at a certain date
+  output GL Listing
 Tools make a big file of transactions
--- tools.rb
+++ tools.rb
@@ -8,6 +8,7 @@ require 'openssl'
 require 'base64'
 require 'jwt'
 require 'httparty'
+require 'mongo'
 
 class Verifier
   def initialize(location, options = {:file => false})
@@ -95,13 +96,63 @@ class Transactioner
 end
 
 
+class Reporter
+  #this obtains teh data from somerhwer and then can output a TB or GL Listing
+  def initialize(location = "mongodb://btxledger:password@ds011705.mlab.com:11705/btxledger", options = {:file => false})
+    if options[:file]
+      #TODO: Read in from file, populat @transactions & @company
+      file = File.read(location)
+      company = JSON.parse(file, :symbolize_names => true)
+    else
+      client = Mongo::Client.new(location)
+      company = client[:ledger]
+      transactions = client[:transactions]
+      @transactions = []
+
+      cursor = company.find
+      cursor.each do |doc|
+        @company = doc
+      end
+
+      cursor = transactions.find
+      cursor.each do |doc|
+        @transactions.push(doc)
+      end
+
+      client.close()
+    end
+  end
+
+  def SaveToFile(location = "OutputCompany.dat")
+    combined =  @company.merge({:Transactions => @transactions})
+    File.open(location,"w"){|f| f.write(combined.to_json)}
+  end
+
+  def GeneralLedger(opts = {})
+    return 0
+  end
+
+  def TrialBalance(opts = {})
+    @accounts = {}
+    @transactions.each do |doc|
+      doc[:Txn][:Postings].each do |line|
+        @accounts[line[:Account].to_sym] ||= 0
+        @accounts[line[:Account].to_sym] += line[:Amt][:Value].to_i
+      end
+    end
+    puts @accounts
+  end
+
+end
 
 def main()
 
   #file = './data/transaction.json'
-  file = './data/x.rb'
-  verifier = Verifier.new(file,{:file => true})
+  #file = './data/x.rb'
+  #verifier = Verifier.new(file,{:file => true})
   binding.pry
+  x = Reporter.new()
+  x.TrialBalance()
 
   #txn_file = File.read('./data/transaction.json')
   #transaction = JSON.parse(txn_file, :symbolize_names => true)
